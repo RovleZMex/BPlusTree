2cc75a841c41a1d7dea9fd89e5cf314b3d4a862c
Santiago <48573143+RovleZMex@users.noreply.github.com>
2023-05-25 12:57:33 -0700

    Added node definition

--- B+Tree.h
+++ B+Tree.h
@@ -7,7 +7,7 @@
 
 #include <iostream>
 
-class BPlusTree{
+class BPlusTree {
 
     /**
      * @brief Class Constructor
@@ -23,15 +23,36 @@ class BPlusTree{
      * @brief Copies Constructor
      * @param other Tree to be copied
      */
-    BPlusTree(const BPlusTree& other);
+    BPlusTree(const BPlusTree &other);
 
     /**
      * @brief Assign operator overload
      * @param other The tree that is being assigned
      * @return A reference to the current tree
      */
-    BPlusTree& operator=(const BPlusTree &other);
+    BPlusTree &operator=(const BPlusTree &other);
 
+private:
+    static const int maxChildren = 3; /**< Maximum children a node can have **/
+
+    struct Node {
+        Node *children[maxChildren + 1] = {}; /**< Array of pointers to the children of the node**/
+        bool isLeaf; /**< True if node is leaf, false otherwise**/
+        Node *parent; /**< Pointer to the parent of the node **/
+        Node *next; /**< Pointer to the right node in the same level **/
+        Node *prev; /**< Pointer to the left node in the same level **/
+
+        /**
+         * @brief Node Constructor
+         */
+        Node() {
+            isLeaf = false;
+            parent = nullptr;
+            next = nullptr;
+            prev = nullptr;
+        }
+
+    } *root /**< Pointer to the root of the tree **/;
 };
 
 #endif //B_TREE_B_TREE_H
